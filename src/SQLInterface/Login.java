5e8379b874be4959b44609f15cc27c3da0b2d3b5
Marc Rousavy <marcrousavy@hotmail.com>
2018-01-26 11:03:53 +0100

    Add Login FX Window

---
+++ src/SQLInterface/Login.java
@@ -0,0 +1,4 @@
+package SQLInterface;
+
+public class Login {
+}
--- src/SQLInterface/Main.java
+++ src/SQLInterface/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package SQLInterface;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login - SQLInterface");
+        primaryStage.setScene(new Scene(root, 750, 500));
         primaryStage.show();
     }
 
