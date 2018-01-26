6d51299589748d1595d65e4054986e4f15547d13
Marc Rousavy <marcrousavy@hotmail.com>
2018-01-26 11:16:59 +0100

    Add SQL Driver

---
+++ src/Driver/Driver.java
@@ -0,0 +1,33 @@
+package Driver;
+
+import JavaLogger.JLogger;
+import JavaLogger.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Driver {
+    private static Logger<String> _logger = new JLogger(System.out);
+
+    private static String buildConnectionString(String server, String database, String user, String password) {
+        String credentials = "?user=" + user + "&password=" + password + "&useSSL=false";
+        return "jdbc:mysql://" + server + "/" + database + credentials;
+    }
+
+    public static Connection connect(String server, String database, String user, String password) throws SQLException {
+        _logger.Log(Logger.Severity.Info, "Connecting...\n");
+        String connectionString = buildConnectionString(server, database, user, password);
+        return DriverManager.getConnection(connectionString);
+    }
+
+    public static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                _logger.Log(ex);
+            }
+        }
+    }
+}
